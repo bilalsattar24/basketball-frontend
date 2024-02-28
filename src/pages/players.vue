fa54d7d94d9c584eaa62d4f1b7d6b7500a5ae6f6
Bilal Sattar <bilalsattar24@gmail.com>
2024-02-28 01:21:52 -0800

    players improvements

--- src/pages/players.vue
+++ src/pages/players.vue
@@ -6,13 +6,21 @@ meta:
 <template>
   <div>
     <h1>Players</h1>
+    <div v-if="isEditing">
+      Edit Player
+      <form @submit.prevent="updatePlayerName">
+        <input type="text" :placeholder="editingPlayerName" />
+        <button type="submit">Save</button>
+      </form>
+      <button @click="isEditing = false">Cancel</button>
+    </div>
     <table>
       <thead>
-        <th>Name</th>
+        <th>Name (Click row to edit player name)</th>
         <th>Team</th>
       </thead>
       <tbody>
-        <tr v-for="player in players" :key="player.id">
+        <tr v-for="player in players" :key="player.id" @click="setEditingMode(player)">
           <td>{{ player.full_name }}</td>
           <td>{{ player.team.name }}</td>
         </tr>
@@ -23,12 +31,20 @@ meta:
 
 <script lang="ts">
 import { usePlayersStore } from '@/stores/playersStore'
+import { Player } from '@/types'
 import { onMounted } from 'vue'
 
 export default {
   name: 'Players',
+  data() {
+    return {
+      isEditing: false,
+      editingPlayerName: '',
+    }
+  },
   setup() {
     const playersStore = usePlayersStore()
+
     onMounted(async () => {
       try {
         await playersStore.fetchPlayers()
@@ -39,6 +55,27 @@ export default {
     return {
       players: playersStore.players,
     }
+  },
+  methods: {
+    setEditingMode(player: Player) {
+      this.isEditing = true
+      this.editingPlayerName = player.full_name
+    },
+    async updatePlayerName(event: Event) {
+      const inputElement = (event.target as HTMLFormElement | null)?.querySelector('input') as HTMLInputElement | null
+      const newValue = inputElement?.value ?? ''
+      const playerToUpdate = this.players.find((p) => p.full_name === this.editingPlayerName)
+      if (playerToUpdate) {
+        try {
+          const playersStore = usePlayersStore()
+          await playersStore.updatePlayerName(playerToUpdate.id, newValue)
+          this.isEditing = false
+          this.editingPlayerName = ''
+        } catch (error) {
+          console.error('Error saving name:', error)
+        }
+      }
+    },
   },
 }
 </script>
